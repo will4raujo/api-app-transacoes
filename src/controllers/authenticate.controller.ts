71b8084fa4fc223176cf08c27a49a97f82b77f88
willian <willian.araujo1994@gmail.com>
2024-06-18 19:36:02 -0300

    feat: validation of authentication requests
    - cors function inserted in main file for local requests
    - created email and password validation in the authentication controller before generating a token

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -1,17 +1,50 @@
-import { Controller, Post } from '@nestjs/common'
+import { Body, Controller, HttpCode, Post, UnauthorizedException, UsePipes } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { compare } from 'bcryptjs'
+import { z } from 'zod'
+import { PrismaService } from '../prisma/prisma.service'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type AuthenticateBody = z.infer<typeof authenticateBodySchema>
 
 @Controller('/sessions')
 export class AuthenticateController {
-  constructor(private jwt: JwtService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwt: JwtService
+  ) {}
 
   @Post()
-  // @HttpCode(201)
-  // @UsePipes(new ZodValidationPipe(createAccountBodySchema))
-  async handle() {
-    const token = this.jwt.sign({ sub: 'user-id' })
-    
-    return token
+  @HttpCode(200)
+  @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle(@Body() body: AuthenticateBody) {
+
+    const { email, password } = authenticateBodySchema.parse(body)
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials')
+    }
+
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      console.log('Invalid credentials')
+      throw new UnauthorizedException('Invalid credentials')
+    }
+
+    const token = this.jwt.sign({ userId: user.id })
+
+    return { token }
+
   }
 }
