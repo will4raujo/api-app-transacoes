1da68a5ea0eeaac76f620c22df4f431445c169d3
willian <willian.araujo1994@gmail.com>
2024-06-18 09:01:40 -0300

    feat: secret token and authenticate controller
    - Implemented sessions Authenticate controller with unique key for a token
    - Created sessions routes for authentications

---
+++ src/controllers/authenticate.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+
+
+@Controller('/sessions')
+export class AuthenticateController {
+  constructor(private jwt: JwtService) {}
+
+  @Post()
+  // @HttpCode(201)
+  // @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle() {
+    const token = this.jwt.sign({ sub: 'user-id' })
+    
+    return token
+  }
+}
